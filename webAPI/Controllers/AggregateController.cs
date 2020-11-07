21a288af2ddc5940cf4f2b9bea9069b15cecadea
Lisa Reynolds <lisamreynolds22@gmail.com>
2020-11-07 13:33:03 -0700

    Basic ping implementation

---
+++ webAPI/Controllers/AggregateController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AggregateController : Controller
+    {
+        // GET api/aggregate/[ip|domain]
+        [HttpGet("{endpoint}")]
+        public ActionResult<string> Get(string endpoint)
+        {
+            return Services.Ping(endpoint);
+        }
+    }
+}
---
+++ webAPI/Services.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webAPI
+{
+    /*
+     * "Send those tasks to a pool of 2+ workers. The API must support workers on separate processes/machines/docker containers than the API."
+     * Just throwing things here for now
+     */
+    public class Services
+    {
+        public static string Ping(string endpoint)
+        {
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+
+            // Use the default Ttl value which is 128,
+            // but change the fragmentation behavior.
+            options.DontFragment = true;
+
+            // Create a buffer of 32 bytes of data to be transmitted.
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120; // ms
+            try
+            {
+                PingReply reply = pingSender.Send(endpoint, timeout, buffer, options);
+                return reply.Status == IPStatus.Success ? "success" : "failure";
+            } catch (Exception e)
+            {
+                return "An error occurred: " + e.InnerException.Message;
+            }
+        }
+    }
+}
